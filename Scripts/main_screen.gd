4bb3f9894026cb1c10434a8753df5288239a7a01
dbrogen <daniel.brogen1@marist.edu>
2025-01-26 16:46:42 -0500

    Adjusted UI to better fit framing changes

--- Scripts/main_screen.gd
+++ Scripts/main_screen.gd
@@ -12,3 +12,7 @@ func _on_timer_timeout() -> void:
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/credits.tscn");
+
+
+func _on_upgrade_key_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/upgrade_key.tscn");
---
+++ Scripts/upgrade_key.gd
@@ -0,0 +1,5 @@
+class_name UpgradeKey
+extends Control
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main_screen.tscn");
